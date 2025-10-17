5dee458cc79b39131b5a21914cd17182db324d86
Khom Socheat <khomsocheat@acledaaib.onmicrosoft.com>
2025-10-17 12:36:36 +0700

    Manage the Components Loop data from array obj then sent props from parent component to child component and uisng the vue slot that have 2 type is default slot that it doesn't need the name and name slot is need name and want to use it call the template to using it

---
+++ src/components/TitleMain.vue
@@ -0,0 +1,23 @@
+<!-- This is component slot reusable when we call it just sent props and use the template that we call it is name slot -->
+<template>
+   <div class="service_container">
+      <div class="container ">
+        <div class="heading_container heading_center">
+          <h2>
+            {{ title }} <span>{{ subtitle }}</span>
+          </h2>
+          <p>
+            <slot name="text"></slot>
+          </p>
+          <div class="d-flex">
+              <slot name="content"></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+  export default{
+    props: ['title','subtitle']
+  }
+</script>
---
+++ src/components/WhyChooseUs.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="box">
+          <div class="img-box">
+            <img :src="icon" alt="">
+          </div>
+          <div class="detail-box">
+            <h5>
+              {{ title }}
+            </h5>
+            <p>
+              {{ description }}
+            </p>
+          </div>
+        </div>
+</template>
+<script>
+  export default{
+    props: ['title','description','icon']
+  }
+</script>
